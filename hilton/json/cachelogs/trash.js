4c69454cb74e81a485ec29f7ccce1c70d73a50d0
ed1868 <ruizeduardo21@gmail.com>
2021-10-15 07:28:42 -0400

    added cache log

---
+++ hilton/json/cachelogs/trash.js
@@ -0,0 +1,12 @@
+let accounts = {
+    chase: "",
+    capital: "",
+    cashApp: " ",
+    dgit: "",
+    date: new Date()
+}
+
+console.log(`ACCOUNTS KEYS: ${Object.keys(accounts)}`);
+console.log(`ACCOUNTS VALUES: ${Object.values(accounts)}`);
+
+
